279b3a67cdf990989e16b835f6487f5b737c8d2e
Asger Bo Lassen <asge4900@edu.campusvejle.dk>
2020-01-07 15:08:25 +0100

    26.09

--- Fasetto.Word.Lib/ViewModel/Application/SettingsViewModel.cs
+++ Fasetto.Word.Lib/ViewModel/Application/SettingsViewModel.cs
@@ -18,6 +18,12 @@ namespace Fasetto.Word.Lib
             // Create commands
             OpenCommand = new RelayCommand(Open);
             CloseCommand = new RelayCommand(Close);
+
+            //TODO: Remove this with real information pulled from our database in future
+            Name = new TextEntryViewModel { Label = "Name", OriginalText = "Luke Malpass" };
+            Username = new TextEntryViewModel { Label = "Username", OriginalText = "luke" };
+            Password = new TextEntryViewModel { Label = "Password", OriginalText = "********" };
+            Email = new TextEntryViewModel { Label = "Email", OriginalText = "contact@angelsix.com" };
         }
 
         #endregion
--- Fasetto.Word/Controls/Input/TextEntryControl.xaml.cs
+++ Fasetto.Word/Controls/Input/TextEntryControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Fasetto.Word
 {
@@ -7,9 +10,58 @@ namespace Fasetto.Word
     /// </summary>
     public partial class TextEntryControl : UserControl
     {
+
+        #region Dependency properties
+
+        /// <summary>
+        /// The label width of the control
+        /// </summary>
+        public GridLength LabelWidth
+        {
+            get => (GridLength)GetValue(LengthProperty);
+            set => SetValue(LengthProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Length.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LengthProperty =
+            DependencyProperty.Register(nameof(LabelWidth), typeof(GridLength), typeof(TextEntryControl), new PropertyMetadata(GridLength.Auto, LabelWidthChangeCallback));        
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default construcor
+        /// </summary>
         public TextEntryControl()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Dependency Callbacks
+
+        /// <summary>
+        /// Called when the label width has changed
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        private static void LabelWidthChangeCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            try
+            {  
+                //Set the column definition width to the new value
+                (d as TextEntryControl).LabelColumnDefinition.Width = (GridLength)e.NewValue;
+            }
+            catch (Exception) 
+            {
+                //Make developer aware of potential issue
+                Debugger.Break();
+
+                (d as TextEntryControl).LabelColumnDefinition.Width = GridLength.Auto;
+            }
+        } 
+        #endregion
     }
 }
